b041a22adba9e8f7fbe653e48ba9ebf69664093a
universenox <khswanson24@gmail.com>
2019-08-09 12:16:40 -0400

    rigid body, seems to be working.

--- driver.m
+++ driver.m
@@ -1,19 +1,20 @@
 % driver
-t0 = 0;
-tf = 1000;
-z0 = [0.1 .01 .001];
+
+
+% z0 = [0 1];
+
 % z(1) = q, z(2) = p
 
 % DHO
 % h = 0.25;
-% % zdot = N(z(t)) - gamma(t) * z(t)
+% zdot = N(z(t)) - gamma(t) * z(t)
 % w = 1;
 % gamma = @(t) 0.005;
 % intgamma = @(a,b) .005 * (b - a); % integral of gamma from a to b
 % N = @(tn, z) [z(2) + gamma(tn) * z(1); -w^2 * z(1) - gamma(tn) * z(2)];
 % % for calculating the actual
 % a = @(t) -gamma(t);
-% b = @(t) sqrt(1 - gamma(t).(t)^2); % for w=1
+% b = @(t) sqrt(1 - gamma(t)^2); % for w=1
 
 % Chaotic pendulum
 % h = 2*pi/22;
@@ -26,18 +27,24 @@ z0 = [0.1 .01 .001];
 % 3D systems are WIP.
 % Lotka Volterra 3d System
 %PARAMETER VALUES
-% h = .1;
-% a = 1; b = 1; c = 1; alpha = 0.02; beta = 0.015; delta = 0.0001;
-% %G = [beta-delta;delta-alpha;alpha-beta]
-% G = [0.025;-0.02;0.0001];
-% M = @(z) [0 a*z(1)*z(2) -b*z(1)*z(3); -a*z(1)*z(2) 0 c*z(2)*z(3); b*z(1)*z(3) -c*z(2)*z(3) 0];
-% J = diag(G);
-% N = @(tn,z) M(z)*[1;1;1];
-% gamma = @(tn) J;
-% intgamma = @(a,b) J * (b-a);
+z0 = [.1 .2 .05];
+h = 1;
+a = 1; b = 1; c = 1; alpha = 0.02; beta = 0.015; delta = 0.0001;
+%G = [beta-delta;delta-alpha;alpha-beta] 
+G = [0.025;-0.02;0.01];
+% G = 0.004*[1 1 1];
+M = @(z) [0 a*z(1)*z(2) -b*z(1)*z(3); -a*z(1)*z(2) 0 c*z(2)*z(3); b*z(1)*z(3) -c*z(2)*z(3) 0];
+J = diag(G);
+N = @(tn,z) M(z)*[1;1;1];
+gamma = @(tn) J;
+intgamma = @(a,b) J * (b-a);
 
-% Rigid Body 3D System
-% I1 = 1; I2 = 1; I3 = 1;
+% % Rigid Body 3D System
+% z0 = [cos(1.1),0,sin(1.1)];
+% h=.3;
+% t0 = 0;
+% tf = 30;
+% I1 = 2; I2 = 1; I3 = 2/3;
 % M = @(z) [0, z(3)/I3, -z(2)/I2;
 %     -z(3)/I3 0 z(1)/I1;
 %     z(2)/I2 -z(1)/I1 0];
@@ -56,39 +63,39 @@ b2 = @(tn) [exp(-gamma(tn) * (h/2))];
 phi2 = @(tn) [exp(-gamma(tn) * (h/2))];
 phi02 = @(tn) [exp(-gamma(tn) * h)];
 
-% %two stage
-% c4 = [1/2 - sqrt(3)/6; 1/2 + sqrt(3)/6];
-% A4 = @(tn) [[1/4, (1/4 - sqrt(3)/6)*exp(sqrt(3)/3 * gamma(tn)*h)]
-%             [(1/4 + sqrt(3)/6)*exp(-sqrt(3)/3 * gamma(tn)*h), 1/4]];
-% b4 = @(tn) [(1/2) * exp(-(1/2 + sqrt(3)/6) * gamma(tn) * h), (1/2) * exp(-(1/2 - sqrt(3)/6) * gamma(tn) * h)];
-% phi4 = @(tn) [[exp(-(1/2 - sqrt(3)/6) * gamma(tn) * h)]
-%               [exp(-(1/2 + sqrt(3)/6) * gamma(tn) * h)]];
-% phi04 = @(tn) [exp(-gamma(tn)*h)];
-% 
-% % three stage
-% c6 = [1/2 - sqrt(15)/10; 1/2; 1/2 + sqrt(15)/10];
-% A6 = @(tn) [5/36, (2/9 - sqrt(15)/15) * exp(intgamma(tn + c6(1)* h,tn + c6(2) * h)), (5/36 - sqrt(15)/30) * exp(intgamma(tn + c6(1) * h, tn + c6(3) * h));
-%      (5/36 + sqrt(15)/24)*exp(intgamma(tn + c6(2) * h, tn+c6(1)* h)), 2/9, (5/36 - sqrt(15)/24)*exp(intgamma(tn + c6(2) * h, tn + c6(3) * h));
-%      (5/36 + sqrt(15)/30)*exp(intgamma(tn + c6(3) * h,tn + c6(1) * h)), (2/9 + sqrt(15)/15)*exp(intgamma(tn + c6(3) * h, tn + c6(2) * h)), 5/36];
-% b6 = @(tn) [(5/18)*exp(-intgamma(tn + c6(1) * h, tn + h)), (4/9)*exp(-intgamma(tn + c6(2)* h, tn + h)), (5/18)*exp(-intgamma(tn + c6(3)*h, tn+ h))];
-% phi6 = @(tn) [exp(-intgamma(tn, tn + c6(1)*h)), exp(-intgamma(tn,tn+c6(2)*h)), exp(-intgamma(tn, tn+c6(3)*h))];
-% phi06 = @(tn) exp(-intgamma(tn, tn + h));
+%two stage
+c4 = [1/2 - sqrt(3)/6; 1/2 + sqrt(3)/6];
+A4 = @(tn) [[1/4, (1/4 - sqrt(3)/6)*exp(sqrt(3)/3 * gamma(tn)*h)]
+            [(1/4 + sqrt(3)/6)*exp(-sqrt(3)/3 * gamma(tn)*h), 1/4]];
+b4 = @(tn) [(1/2) * exp(-(1/2 + sqrt(3)/6) * gamma(tn) * h), (1/2) * exp(-(1/2 - sqrt(3)/6) * gamma(tn) * h)];
+phi4 = @(tn) [[exp(-(1/2 - sqrt(3)/6) * gamma(tn) * h)]
+              [exp(-(1/2 + sqrt(3)/6) * gamma(tn) * h)]];
+phi04 = @(tn) [exp(-gamma(tn)*h)];
+
+% three stage
+c6 = [1/2 - sqrt(15)/10; 1/2; 1/2 + sqrt(15)/10];
+A6 = @(tn) [5/36, (2/9 - sqrt(15)/15) * exp(intgamma(tn + c6(1)* h,tn + c6(2) * h)), (5/36 - sqrt(15)/30) * exp(intgamma(tn + c6(1) * h, tn + c6(3) * h));
+     (5/36 + sqrt(15)/24)*exp(intgamma(tn + c6(2) * h, tn+c6(1)* h)), 2/9, (5/36 - sqrt(15)/24)*exp(intgamma(tn + c6(2) * h, tn + c6(3) * h));
+     (5/36 + sqrt(15)/30)*exp(intgamma(tn + c6(3) * h,tn + c6(1) * h)), (2/9 + sqrt(15)/15)*exp(intgamma(tn + c6(3) * h, tn + c6(2) * h)), 5/36];
+b6 = @(tn) [(5/18)*exp(-intgamma(tn + c6(1) * h, tn + h)), (4/9)*exp(-intgamma(tn + c6(2)* h, tn + h)), (5/18)*exp(-intgamma(tn + c6(3)*h, tn+ h))];
+phi6 = @(tn) [exp(-intgamma(tn, tn + c6(1)*h)), exp(-intgamma(tn,tn+c6(2)*h)), exp(-intgamma(tn, tn+c6(3)*h))];
+phi06 = @(tn) exp(-intgamma(tn, tn + h));
 
 
-t = t0:h:tf;
 [~, z2, ~] = exponentialRK(N, gamma, A2, b2, phi2, phi02, [t0 tf], z0, h);
-% [~, z4, ~] = exponentialRK(N, gamma, A4, b4, phi4, phi04, [t0 tf], z0, h);
+[~, z4, ~] = exponentialRK(N, gamma, A4, b4, phi4, phi04, [t0 tf], z0, h);
 % [~, z6, ~] = exponentialRK(N, gamma, A6, b6, phi6, phi06, [t0 tf], z0, h);
 
 
-% hs = [.01:.01:.1]; % we'll try these h's to make our graphs w.r.t step size
-% err_step = zeros(3,size(hs, 2)); % 3 methods stages 1,2,3, and n step sizes
-% % 2nd order IFRK based on implicit midpoint rule
+hs = [.01:.01:.1]; % we'll try these h's to make our graphs w.r.t step size
+err_step = zeros(3,size(hs, 2)); % 3 methods stages 1,2,3, and n step sizes
+% 2nd order IFRK based on implicit midpoint rule
 % for i = 1 : size(hs,2)
 %     h = hs(1,i);
-%     
+% 
 %     [~, z2, ~] = exponentialRK(N, gamma, A2, b2, phi2, phi02, [t0 tf], z0, h);
 %     [~, z4, ~] = exponentialRK(N, gamma, A4, b4, phi4, phi04, [t0 tf], z0, h);
+%     [~, z6, ~] = exponentialRK(N, gamma, A6, b6, phi6, phi06, [t0 tf], z0, h);
 % 
 %     t = [t0:h:tf];
 %     actual = exp(a(t) .* t) ./ b(t) .* sin(b(t) .* t);
@@ -96,6 +103,7 @@ t = t0:h:tf;
 % 
 %     err_step(1, i) = abs(z2(2,1) - actual(2))/h;
 %     err_step(2, i) = abs(z4(2,1) - actual(2))/h;
+%     err_step(3, i) = abs(z6(2,1) - actual(2))/h;
 % end
 
 
--- grapher.m
+++ grapher.m
@@ -3,6 +3,23 @@ clf
 t = t0:h:tf;
 t = t';
 
+% for Rigid body
+% seems Casimir and Energy error stays the same regardless of stages in method
+% C0 = z0(1)^2 + z0(2)^2 + z0(3)^2;
+% Cn2 = z2(:,1).^2 + z2(:,2).^2 + z2(:,3).^2;
+% 
+% Cn4 = z4(:,1).^2 + z4(:,2).^2 + z4(:,3).^2;
+% plot(t,abs(Cn2 - C0*ones(size(Cn4))), 'DisplayName', 'E_n');
+% title('Casimir Error, C(z) = z_1^2 + z_2^2 + z_3^2')
+% ylabel('C(z_n) - C(z_0)')
+% xlabel('t')
+% 
+% rigid body energy
+% H0 = 1/2 * (z0(1).^2/I1 + z0(2).^2/I2 + z0(3).^2/I3);
+% Hn2 = 1/2 * (z2(:,1).^2/I1 + z2(:,2).^2/I2 + z2(:,3).^2/I3);
+% Hn4 = 1/2 * (z4(:,1).^2/I1 + z4(:,2).^2/I2 + z4(:,3).^2/I3);
+% plot(t, abs(Hn4 - H0 * ones(size(Hn2))), 'DisplayName', '\epsilon_n');
+
 % % exact
 % B = @(t) sqrt(w^2 - gamma(t)^2);
 % A = @(t) [cos(B(t) * t) + gamma(t)/B(t) * sin(B(t) * t), 1/B(t) * sin(B(t) * t);
@@ -16,14 +33,13 @@ t = t';
 % ylabel('err')
 % xlabel('t')
 
-
 % Damped Harmonic Oscillator Dissipation,
 % See Fig. 3, Bhatt Floyd Moore '16 J. Sci. Comput
 % Expect to stay near 0 for S-P methods.
-plot(t, log(abs(z2(:,1))) + gamma(t(:)) * t(:));
-title('DHO dissipation')
-ylabel('d(t)')
-xlabel('t')
+% plot(t, log(abs(z2(:,1))) + gamma(t(:)) * t(:));
+% title('DHO dissipation')
+% ylabel('d(t)')
+% xlabel('t')
 
 % abs error
 %semilogy(t, abs(u2(:,1) - actual), 'DisplayName', '1s');
@@ -38,12 +54,21 @@ xlabel('t')
 % plot(t, u4(:,1))
 % plot(t, sin(t))
 
-% loglog(hs, err_step(1,:), 'x-', 'DisplayName', '1-stage');
+% loglog(hs, err_step(1,:), 'DisplayName', '1-stage');
 % hold on
-% loglog(hs, err_step(2,:), 'o-', 'DisplayName', '2-stage');
+% loglog(hs, err_step(2,:), 'DisplayName', '2-stage');
+% loglog(hs, err_step(3,:), 'DisplayName', '3-stage');
+% 
+% % best fit slope chekc
+% Bp = polyfit(log10(hs(2:size(hs,2))), log10(err_step(3,2:size(hs,2))), 1)
+% Yp = polyval(Bp, log10(hs));
+% Yp2 = 10.^(Yp);
+% 
+% plot(hs, Yp2, '-r', 'DisplayName', 'Fit');
+% text(1.1, 10, strcat('Slope in Log-Log Space =', num2str(Bp(1))), 'Interpreter', 'none');
 % 
 % title('error, IFRK, for stage 1,2')
 % ylabel('local abs error /h')
 % xlabel('h')
-% 
+
 legend
